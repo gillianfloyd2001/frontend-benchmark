e22aba78362fcfa49d8ea65302161692857c0c45
Student <student@donation.local>
2019-10-18 16:10:44 -0500

    fixing it

--- static/base.js
+++ static/base.js
@@ -1,2 +1,20 @@
-const button = document.querySelector(".like");
+const button = document.querySelector("#like");
+const entryInput = document.querySelector(".input__content")
 
+function updateButtonList(data) {
+    const options = [];
+    for (const name of data.buttons) {
+        options.push(`<option value="${name}">`);
+        button.innerHTML = options.join('');
+    };
+};
+
+button.addEventListener("click", () => {
+    console.log("hi")
+    const entryId = document.querySelector('#like');
+    const url = `entries/${entryId}/like`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(updateButtonList);
+});
